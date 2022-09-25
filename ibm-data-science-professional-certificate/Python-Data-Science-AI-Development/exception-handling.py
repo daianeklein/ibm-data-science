2068d2ffa69f6ae3e8b60078d71f99a2173d2d96
Daiane Klein <daianeklein@MacBook-Air-de-Daiane.local>
2022-09-25 19:10:33 -0300

    add example 02

--- ibm-data-science-professional-certificate/Python-Data-Science-AI-Development/exception-handling.py
+++ ibm-data-science-professional-certificate/Python-Data-Science-AI-Development/exception-handling.py
@@ -1,5 +1,5 @@
 try:
-    #a = {}
+    a = {}
     print(a)
 except NameError as erro:
     print('Developer error')
@@ -8,6 +8,22 @@ except (IndexError, KeyError) as erro:
 except Exception as erro:
     print('unexpected error!')
 else:
+    print('Sucess!')
+finally:
     pass
 
-    print('Keep going')
+print('Keep going')
+
+
+## example 02
+try:
+    b = 'hey'
+    #a = 1/0
+except NameError as erro:
+    print('erro 01')
+else:
+    pass
+finally:
+    a = None
+
+print(a)
